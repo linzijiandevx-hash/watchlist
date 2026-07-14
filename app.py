ed34d309b2adcd51ff022f66c67334778696a645
linzijiandevx-hash <linzijiandevx@gamil.com>
2026-07-14 10:49:02 +0800

    Add the index page template

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
+from flask import url_for
+
+name = "lzj"
+movies = [
+    {"title": "Totoro", "year": 1988},
+    {"title": "Ponyo", "year": 2008},
+    {"title": "Spirited Away", "year": 2001},
+]
+
 
 app = Flask(__name__)
+
 @app.route('/')
-def hello():
-    return '<h1>Hello Totoro</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 # 一个函数可以绑定多个路由
 @app.route('/index')
 @app.route('/home')
 def hello_home():
     return '<h1>HelloHome</h1>'
-    
-@app.route('/user/<username>')
-def hello_user(username):
-    return f'<h1>HelloUser: {username}</h1>'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
 
 @app.route('/test')
 def test_url_for():
-    print(url_for('hello'))
-    print(url_for('user_page', username='totoro'))
-   
+    # 下面是一些调用示例（访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
